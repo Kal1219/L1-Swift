05d40c68e9c677b0e5f8652906a320d52a5a5e58
Kal1219 <kaleb.cortes1208@gmail.com>
2023-03-17 13:55:24 -0600

    Initial Commit

---
+++ L1 Swift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  L1 Swift
+//
+//  Created by Kaleb on 17/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ L1 Swift/L1_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_SwiftApp.swift
+//  L1 Swift
+//
+//  Created by Kaleb on 17/03/23.
+//
+
+import SwiftUI
+
+@main
+struct L1_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
