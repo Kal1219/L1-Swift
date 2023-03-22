cce4080c68a55c527f046ed3ada25201a2c206d5
Kal1219 <kaleb.cortes1208@gmail.com>
2023-03-22 11:56:47 -0600

    Le añado una imagen de fondo con diferentes atributos y un título igual con diferentes atributos

--- L1 Swift/ContentView.swift
+++ L1 Swift/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack
+        {
+            Image("fondo")
+                .resizable()
+                .cornerRadius(15)
+                .aspectRatio(contentMode: .fit)
+                .padding()
+            Text("Fondo de pantalla")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
         }
-        .padding()
+        
+        
+
+        
     }
 }
 
